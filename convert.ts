41a81004562b000b043e7ac4b2ee839a86842b39
Eejit <76887639+Eejit43@users.noreply.github.com>
2023-12-28 18:00:40 -0500

    feat: rename directories, add more icons

--- convert.ts
+++ convert.ts
@@ -3,20 +3,20 @@ import sharp from 'sharp';
 
 const colors = (await Bun.file('colors.json').json()) as { default: string; icons: Record<string, string> };
 
-for (const icon of readdirSync('assets/svgs/multicolored')) {
-    const fileContent = await Bun.file(`assets/svgs/multicolored/${icon}`).arrayBuffer(); // eslint-disable-line no-await-in-loop
+for (const icon of readdirSync('assets/svgs/other')) {
+    const fileContent = await Bun.file(`assets/svgs/other/${icon}`).arrayBuffer(); // eslint-disable-line no-await-in-loop
 
     sharp(fileContent)
         .resize({ width: 500, height: 500, fit: 'contain', background: { r: 0, g: 0, b: 0, alpha: 0 } })
-        .toFile(`assets/pngs/multicolored/${icon.replace('.svg', '.png')}`);
+        .toFile(`assets/pngs/other/${icon.replace('.svg', '.png')}`);
 }
 
-for (const icon of readdirSync('assets/svgs/single-colored')) {
-    let fileContent = await Bun.file(`assets/svgs/single-colored/${icon}`).text(); // eslint-disable-line no-await-in-loop
+for (const icon of readdirSync('assets/svgs/from-discord')) {
+    let fileContent = await Bun.file(`assets/svgs/from-discord/${icon}`).text(); // eslint-disable-line no-await-in-loop
 
     fileContent = fileContent.replace('<svg', `<svg style="color: hsl(${colors.icons[icon.replace('.svg', '')] ?? colors.default})"`);
 
     sharp(Buffer.from(fileContent))
         .resize({ width: 500, height: 500, fit: 'contain', background: { r: 0, g: 0, b: 0, alpha: 0 } })
-        .toFile(`assets/pngs/single-colored/${icon.replace('.svg', '.png')}`);
+        .toFile(`assets/pngs/from-discord/${icon.replace('.svg', '.png')}`);
 }
