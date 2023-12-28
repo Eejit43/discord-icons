30308a918a1b733cd28614f0ef07191990becdb3
Eejit <76887639+Eejit43@users.noreply.github.com>
2023-12-27 22:31:40 -0500

    feat: add conversion script

--- convert.ts
+++ convert.ts
@@ -1,1 +1,12 @@
-console.log('Hello world!');
+import { readdirSync } from 'node:fs';
+import sharp from 'sharp';
+
+for (const file of readdirSync('assets/svgs')) {
+    let fileContent = await Bun.file(`assets/svgs/${file}`).text();
+
+    fileContent = fileContent.replace('<svg', '<svg style="color: #b5bac1"');
+
+    sharp(Buffer.from(fileContent))
+        .resize(500)
+        .toFile(`assets/pngs/${file.replace('.svg', '.png')}`);
+}
