fbd5665db46e2f0372b93b43ffd60e1133535991
Eejit <76887639+Eejit43@users.noreply.github.com>
2023-12-28 23:25:51 -0500

    feat: fix audio-muted color, alphabetize list

--- colors.ts
+++ colors.ts
@@ -6,29 +6,29 @@ const defaults = {
 
 export default {
     'announcement-channel': defaults['channel-icon'],
-    'audio-max': defaults['primary-300'],
-    bell: defaults['interactive-normal'],
-    emoji: defaults['interactive-normal'],
-    folder: defaults['interactive-normal'], // Not an actually assigned color
+    'audio-muted': defaults['primary-300'],
+    'bell': defaults['interactive-normal'],
+    'calendar': defaults['interactive-normal'],
+    'clock': defaults['interactive-normal'],
+    'command': defaults['interactive-normal'],
+    'email': defaults['interactive-normal'],
+    'emoji': defaults['interactive-normal'],
+    'folder': defaults['interactive-normal'], // Not an actually assigned color
+    'forum-channel': defaults['channel-icon'],
+    'location': defaults['interactive-normal'],
+    'mention': defaults['interactive-normal'],
     'owner-crown': '40 86.4% 56.9%',
-    pin: defaults['interactive-normal'],
+    'people': defaults['interactive-normal'],
+    'person': defaults['interactive-normal'],
+    'pin': defaults['interactive-normal'],
+    'role': defaults['interactive-normal'], // Not an actually assigned color
     'rules-channel': defaults['channel-icon'],
     'server-booster': '302 100% 72.5%',
     'stage-channel': defaults['channel-icon'],
-    sticker: defaults['interactive-normal'],
+    'sticker': defaults['interactive-normal'],
     'text-channel': defaults['channel-icon'],
+    'text': defaults['interactive-normal'],
+    'thread': defaults['channel-icon'], // For standardization, uses slightly different colors
     'voice-channel': defaults['channel-icon'],
-    'forum-channel': defaults['channel-icon'],
-    warning: null,
-    email: defaults['interactive-normal'],
-    person: defaults['interactive-normal'],
-    role: defaults['interactive-normal'], // Not an actually assigned color
-    mention: defaults['interactive-normal'],
-    text: defaults['interactive-normal'],
-    people: defaults['interactive-normal'],
-    location: defaults['interactive-normal'],
-    calendar: defaults['interactive-normal'],
-    command: defaults['interactive-normal'],
-    clock: defaults['interactive-normal'],
-    thread: defaults['channel-icon'], // For standardization, uses slightly different colors
+    'warning': null,
 } as Record<string, string | null>;
