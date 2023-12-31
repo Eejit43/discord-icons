f76af3e44f739b22a5db005e65b1f093f870ed85
Eejit <76887639+Eejit43@users.noreply.github.com>
2023-12-31 14:51:02 -0500

    feat: add new emojis

--- README.md
+++ README.md
@@ -2,4 +2,4 @@
 
 This repository contains a collection of Discord icon assets in SVGs and PNGs. I do not own the rights to any such assets, and the LICENSE only applies to script files in this repository.
 
-As some icons are no longer SVGs in Discord, I get these icons from the Figma file ["Discord Collection" by NEVER See](https://www.figma.com/community/file/1248627030152531064).
+As some icons are no longer SVGs in Discord, I get these icons from the Figma file ["Discord Collection" by NEVER See](https://www.figma.com/community/file/1248627030152531064). These icons always exist in the "other" directories.
--- colors.ts
+++ colors.ts
@@ -15,7 +15,10 @@ export const colors = {
         - "error" doesn't actually exist, it is simply a recolored version of "warning"
         - "image" doesn't actually exist, it is simply a recolored version of "media-channel"
         - "cross" doesn't actually have white in the app, it was added for consistency with "check"
-        - "thread" doesn't actually use the channel icon color, this is used for consistency with other channel icons
+        - "thread" doesn't actually use the "channel-icon" color, this is used for consistency with other channel icons
+        - "flag" doesn't actually use the "interactive-normal" color, this is for consistency with other emojis
+        - "flag" doesn't actually use the "interactive-normal" color, this is for consistency with other emojis
+        - "text-channel-nsfw" uses the "interactive-normal" color instead of the "channel-icon" color for consistency with other emojis where it is used
  */
 
 export const overrides = {
@@ -26,6 +29,7 @@ export const overrides = {
     'forum-channel': colors['channel-icon'],
     'media-channel': colors['channel-icon'],
     'owner-crown': '40 86.4% 56.9%',
+    'ping-green': '145 65% 39.2%',
     'rules-channel': colors['channel-icon'],
     'server-booster': '302 100% 72.5%',
     'stage-channel': colors['channel-icon'],
